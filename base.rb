4acfbd407e55e8e3259625014ca0760e19b3dc34
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-10 10:51:42 +0900

    add methods

--- base.rb
+++ base.rb
@@ -10,7 +10,40 @@ def do_something3(a: b)
   
 end
 
+def aaaaaaaaaaa(a, *b, **c)
+  
+end
+
+def ccccccccc(a, b = 1, c: 1)
+  
+end
+
+def ddddddddd(x: 1, y: 2, z: 3)
+  
+end
+
+def qqqqqqqqqqq(x)
+  
+end
+
+def multi_line_arg(
+  x,
+  y,
+  fdjsaflk
+)
+  
+end
+
 
 do_something
 do_something2(x, y)
 do_something3(a: z)
+
+
+aaaaaaaaaaa(1, 2, a: b)
+ccccccccc(a)
+ddddddddd(x: 2)
+ddddddddd(y: 435)
+qqqqqqqqqqq(20)
+
+multi_line_arg(x, y, fdjsaflk)
