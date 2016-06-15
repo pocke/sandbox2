093ac7cb93eeac5fba6c2fb3ebcf342ba6dfff7a
SideCI <support@sideci.com>
2016-06-15 02:41:54 +0000

    Applied fixes from SideCI

--- test.rb
+++ test.rb
@@ -1,21 +1,19 @@
-a = "bbb"
+a = 'bbb'
 
 def foo
-    'a'
+  'a'
 end
 
-aaa;
+aaa
 
-not 1
+!1
 
-l = lambda {|a| a }
-l.(x)
+l = ->(a) { a }
+l.call(x)
 l[x]
 l.call(x)
 
 def b(x)
-  
-end 
+end
 
-
-[a,b,c,d,e]
+[a, b, c, d, e]
