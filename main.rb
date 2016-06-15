de62ce0867b4067f771ef9e8a5403cefe3c2c84b
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-06-15 16:51:16 +0900

    long diff

--- main.rb
+++ main.rb
@@ -10,3 +10,87 @@ def baz
   a = "aaa"
   return a
 end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
+def foo
+  a = "aaa"
+  return a
+end
+def bar
+  a = "aaa"
+  return a
+end
+def baz
+  a = "aaa"
+  return a
+end
