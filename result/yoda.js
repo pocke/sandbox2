2440dc9016061657568b88e4001a5583a717cf0e
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-20 15:55:04 +0900

    Un comment

--- result/yoda.js
+++ result/yoda.js
@@ -1,19 +1,19 @@
-//
-// /*eslint no-multi-spaces: "error"*/
-//
-// var a =  1;
-// var a =  1;
-// var a =  1;
-//
-// if(foo   === "bar") {}
-// if(foo   === "bar") {}
-//
-// a <<  b
-// a <<  b
-//
-// var arr = [1,  2];
-//
-// a ?  b: c
-//
-// var a =  1;
-// var a =  1;
+
+/*eslint no-multi-spaces: "error"*/
+
+var a =  1;
+var a =  1;
+var a =  1;
+
+if(foo   === "bar") {}
+if(foo   === "bar") {}
+
+a <<  b
+a <<  b
+
+var arr = [1,  2];
+
+a ?  b: c
+
+var a =  1;
+var a =  1;
