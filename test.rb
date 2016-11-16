8132a5dea6db8b779598042a587cbd7704e3f4b1
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-11-16 16:06:00 +0900

    a

--- test.rb
+++ test.rb
@@ -4,15 +4,6 @@ def foo
     'a'
 end
 
-def a
-  def b
-    1
-    ['a','c']
-    'C'
-    "A"
-    ?a
-  end
-end
 
 aaa;
 
@@ -22,15 +13,3 @@ not 1
  l.(x)
 l[x]
 l.call(x)
-
-  def b(x)
-    
-  end 
-
-
-  ['x','b',"c"]
-    [x,a,b,c,d,e]
-    
-    foo (  x)
-
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa .()
