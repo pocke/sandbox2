31401d148c87f8dc44e556c448e6450e133dea7a
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-11-16 15:28:46 +0900

    dirty

--- test.rb
+++ test.rb
@@ -8,16 +8,18 @@ aaa;
 
 not 1
 
-l = lambda {|a| a }
-l.(x)
+ l = lambda {|a| a }
+ l.(x)
 l[x]
 l.call(x)
 
-def b(x)
-  
-end 
+  def b(x)
+    
+  end 
 
 
     [a,b,c,d,e]
     
     foo (  x)
+
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa .()
