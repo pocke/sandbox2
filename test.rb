7fefa304c6e119549120ec78bda12db2306f8ba2
Masataka Kuwabara <kuwabara@pocke.me>
2017-03-15 19:20:29 +0900

    Update

--- test.rb
+++ test.rb
@@ -53,3 +53,58 @@ def foo
   x = 3
   4
 end
+def foo
+  1
+  2
+  2
+  2
+  2
+  2
+  2
+  2
+  2
+  if cond
+    if cond
+      if cond
+        1
+  if cond
+    if cond
+      if cond
+        1
+      end
+    end
+  end
+  if cond
+    if cond
+      if cond
+        1
+      end
+    end
+  end
+  if cond
+    if cond
+      if cond
+        1
+      end
+    end
+  end
+  if cond
+    if cond
+      if cond
+        1
+      end
+    end
+  end
+  if cond
+    if cond
+      if cond
+        1
+      end
+    end
+  end
+      end
+    end
+  end
+  x = 3
+  4
+end
